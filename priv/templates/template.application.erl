a23155efa96ce03be91a2fa325b349cf025262da
Pablo Vieytes <pvieytes@openshine.com>
2013-05-27 15:49:13 +0200

    update application template

--- priv/templates/template.application.erl
+++ priv/templates/template.application.erl
@@ -12,8 +12,6 @@
 	 test_view/3,
 	 upload_view/3]).
 
--record(state, {}).
-
 %% minino funs
 
 init(_MConf) ->
@@ -23,22 +21,17 @@ dispatch_rules() ->
     [%% {Id::atom(), Path::[string()|atom()], view::atom()}
      {root_page, [], home_view},
      {home_page, ["home"], home_view},
-     {test_page, ["test", testvalue], test_view},
-     {upload_page, ["upload"], upload_view}
+     {test_page, ["test", testvalue], test_view}
     ].
 
 
 %% views
 
-home_view(MReq, _Args, _State) ->
+home_view(MReq, _Args, _Term) ->
     {ok, Html} = minino_api:render_template("home.html", [{text, "Meow!!"}]),
     minino_api:response(Html, MReq).
 
-test_view(MReq, Args, _State) ->
+test_view(MReq, Args, _Term) ->
     TestVal = proplists:get_value(testvalue, Args),
     Html = lists:flatten(io_lib:format("<html><body>test: ~s</body></html>", [TestVal])),
-    minino_api:response(Html, MReq).
-
-upload_view(MReq, _Args, _State) ->
-    {ok, Html} = minino_api:render_template("uploadfile.html", []),
     minino_api:response(Html, MReq).
